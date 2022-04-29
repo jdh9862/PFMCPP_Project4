2af76ce42fc46866c71b369751a9359ba6b21cce
jdh9862 <jdh9862@gmail.com>
2022-04-28 21:58:04 -0700

    Resolve merge issues

--- main.cpp
+++ main.cpp
@@ -27,30 +27,30 @@ Do not delete your previous main.
  5) delete the example below after it makes sense how your code will change due to 1).
  */
 
-#include <iostream>
-namespace Example
-{
-    int main()
-    {
-        FloatType floatNum(4.3f);
-        IntType intNum(2);
-        IntType intNum2(6);
-
-        /*
-        if you previously had a line like this demonstrating chaining:
-
-            intNum.add(3).add(4.5f).divide(floatNum);
-
-        it should become:
-        */
-        intNum += 3;
-        intNum += 4.5f;
-        intNum /= floatNum;
-        std::cout << "intNum: " << intNum << std::endl;
-
-        return 0;
-    }
-}
+//#include <iostream>
+//namespace Example
+//{
+//    int main()
+//    {
+//        FloatType floatNum(4.3f);
+//        IntType intNum(2);
+//        IntType intNum2(6);
+//
+//        /*
+//        if you previously had a line like this demonstrating chaining:
+//
+//            intNum.add(3).add(4.5f).divide(floatNum);
+//
+//        it should become:
+//        */
+//        intNum += 3;
+//        intNum += 4.5f;
+//        intNum /= floatNum;
+//        std::cout << "intNum: " << intNum << std::endl;
+//
+//        return 0;
+//    }
+//}
 
  /*
  6) compile/link/run to make sure you don't have any errors or warnings.
@@ -478,15 +478,18 @@ void part3()
     IntType it ( 34 );
     DoubleType pi( 3.14 );
 
-    std::cout << "The result of FloatType^4 divided by IntType is: " << ft.multiply( ft ).multiply( ft ).divide( static_cast<float>(it) ) << std::endl;
-    std::cout << "The result of DoubleType times 3 plus IntType is : " << dt.multiply( 3 ).add( it ) << std::endl;
+    std::cout << "The result of FloatType^4 divided by IntType is: "
+              << ((((ft *= ft) *= ft) *= ft) /= static_cast<float>(it)) << std::endl;
+    std::cout << "The result of DoubleType times 3 plus IntType is : " << ((dt *= 3) += it) << std::endl;
     std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: "
-              << static_cast<int>( it.divide( static_cast<int>(pi) ).multiply( static_cast<int>(dt) ).subtract( static_cast<int>(ft) ) ) << std::endl;
+              << (static_cast<int>(((it /= static_cast<int>(pi)) *= static_cast<int>(dt)) -= static_cast<int>(ft)))
+              << std::endl;
     std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
-    std::cout << it.multiply(it).divide(0).divide(0.0f).divide(0.0) << std::endl;
+    std::cout << ((((it *= it) /= 0) /= 0.0f) /= 0.0) << std::endl;
 
-    std::cout << "FloatType x IntType  =  " << it.multiply( static_cast<int>(ft) ) << std::endl;
-    std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it.add( static_cast<int>(dt) ).add( static_cast<int>(ft) ).multiply( 24 ) << std::endl;
+    std::cout << "FloatType x IntType  =  " << (it *= static_cast<int>(ft)) << std::endl;
+    std::cout << "(IntType + DoubleType + FloatType) x 24 = "
+              << (((it += static_cast<int>(dt)) += static_cast<int>(ft)) *= 24) << std::endl;
 }
 
 void part4()
@@ -611,7 +614,7 @@ int main()
     std::cout << "Initial value of it: " << it << std::endl;
     // --------
     std::cout << "Use of function concatenation (mixed type arguments) " << std::endl;
-    std::cout << "New value of dt = (dt * it) / 5.0f + ft = " << ((((dt *= it) /= 5.0) += double(ft))) << std::endl;
+    std::cout << "New value of dt = (dt * it) / 5.0f + ft = " << ((((dt *= it) /= 5.0) += static_cast<double>(ft))) << std::endl;
 
     std::cout << "---------------------\n" << std::endl; 
     
