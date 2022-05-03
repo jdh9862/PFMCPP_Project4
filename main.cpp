6081490d85859fb0428b50038cc96719d3647756
jdh9862 <jdh9862@gmail.com>
2022-05-03 09:24:58 -0700

    Use Temporary

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-Project 4: Part 8 / 9
+Prject 4: Part 8 / 9
  video: Chapter 5 Part 6 Task
 
 Create a branch named Part8
@@ -32,21 +32,6 @@ Create a branch named Part8
  3) You'll need to template your overloaded math operator functions in your Templated Class from Ch5 p04
     use static_cast to convert whatever type is passed in to your template's NumericType before performing the +=, -=, etc.  here's an example implementation:
  */
-//namespace example
-//{
-//template<typename NumericType>
-//struct Numeric
-//{
-//    //snip
-//    template<typename OtherType>
-//    Numeric& operator-=(const OtherType& o)
-//    {
-//        *value -= static_cast<NumericType>(o);
-//        return *this;
-//    }
-//    //snip
-//};
-//}
 
 /*
  4) remove your specialized <double> template of your Numeric<T> class from the previous task (ch5 p04)
@@ -65,6 +50,21 @@ Create a branch named Part8
     See the previous hint for implementing the conversion functions for the Temporary if you want to get the held value
 */
 
+/*namespace example
+{
+template<typename NumericType>
+struct Numeric
+{
+    //snip
+    template<typename OtherType>
+    Numeric& operator-=(const OtherType& o)
+    {
+        *value -= static_cast<NumericType>(o);
+        return *this;
+    }
+    //snip
+};
+}*/
 
 /*
  7) replace main() with the main below
@@ -136,7 +136,7 @@ struct Temporary
      revise these conversion functions to read/write to 'v' here
      hint: what qualifier do read-only functions usually have?
      */
-    operator NumericType const () { return v; }
+    operator NumericType() const { return v; } //read-only
     operator NumericType&() { return v; }
 private:
     static int counter;
@@ -149,82 +149,87 @@ int Temporary<NumericType>::counter = 0;
 template<typename T>
 struct Numeric
 {
-    using Type = T;
+    using Type = Temporary<T>;
 
     explicit Numeric(Type lhs) : value(std::make_unique<Type>(lhs)) {}
 
     template<typename Param>
-    Numeric<Type>& operator=( const Param& rhs )
+    Numeric& operator=( const Param& rhs )
     {
-        *value = static_cast<Type>(rhs);
+        *value = static_cast<T>(rhs);
         return *this;
     }
 
     template<class Param>
-    Numeric<Type>& operator+=(const Param& rhs)
+    Numeric& operator+=(const Param& rhs)
     {
-        *value += static_cast<Type>(rhs);
+        *value += static_cast<T>(rhs);
         return *this;
     }
 
     template<class Param>
-    Numeric<Type>& operator-=(const Param& rhs)
+    Numeric& operator-=(const Param& rhs)
     {
-        *value -= static_cast<Type>(rhs);
+        *value -= static_cast<T>(rhs);
         return *this;
     }
 
-    template<class Param>
-    Numeric<Type>& operator*=(const Param& rhs)
+    template<typename ParamType>
+    Numeric& operator*=( const ParamType& rhs )
     {
-        *value *= static_cast<Type>(rhs);
+        *value *= static_cast<T>(rhs);
         return *this;
     }
 
     template<typename Param>
-    Numeric<Type>& operator/=(const Param& rhs )
+    Numeric& operator/=( const Param rhs )
     {
         if (std::is_same<Type, int>::value)
         {
             if (std::is_same<Param, int>::value)
             {
-                if(std::abs(rhs) <= std::numeric_limits<Type>::epsilon())
+                if(std::abs(rhs) <= std::numeric_limits<Param>::epsilon())
                 {
                     std::cout << "error: integer division by zero is an error and will crash the program!\n";
                     return *this;
                 }
             }
-            else if (std::abs(rhs) <= std::numeric_limits<Type>::epsilon())
+            else if (std::abs(rhs) <= std::numeric_limits<Param>::epsilon())
             {
                 std::cout << "can't divide integers by zero!\n";
                 return *this;
             }
         }
-        else if (std::abs(rhs) <= std::numeric_limits<Type>::epsilon())
+        else if (std::abs(rhs) <= std::numeric_limits<Param>::epsilon())
         {
             std::cout << "warning: floating point division by zero!" << std::endl;
         }
 
-        *value /= rhs;
+        *value /= static_cast<T>(rhs);
         return *this;
     }
 
-    operator Type() const
+    operator T() const
     {
-        return static_cast<Type>(*value);
+        return *value;
+    }
+
+    operator T&()
+    {
+        return *value;
     }
 
     template<class Param>
-    Numeric<Type>& pow(const Param& rhs)
+    Numeric& pow(const Param& rhs)
     {
-        *value = static_cast<Type>(std::pow(*value, static_cast<Type>(rhs)));
+        *value = static_cast<T>(std::pow( *value, static_cast<T>(rhs) ));
         return *this;
     }
 
     template<class Func>
-    Numeric<Type>& apply(Func func)
+    Numeric& apply(Func func)
     {
-        func(*value);
+        func(value);
         return *this;
     }
 
@@ -261,127 +266,11 @@ private:
     float x{0}, y{0};
 };
 
-void part3()
-{
-    Numeric<float> ft( 5.5f );
-    Numeric<double> dt( 11.1 );
-    Numeric<int> it ( 34 );
-    Numeric<double> pi( 3.14 );
-
-    ft *= ft;
-    ft *= ft;
-    ft /= it;
-    std::cout << "The result of FloatType^4 divided by IntType is: " <<  ft << std::endl;
-    dt *= 3;
-    dt += it;
-    std::cout << "The result of DoubleType times 3 plus IntType is : " <<  dt << std::endl;
-    it /= pi;
-    it *= dt;
-    it -= ft;
-    std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << static_cast<int>(it) << std::endl;
-    std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
-    it *= it;
-    it /= 0;
-    it /= 0.0f;
-    it /= 0.0;
-    std::cout <<  it << std::endl;
-
-    it *= ft;
-    std::cout << "FloatType x IntType  =  " <<  it << std::endl;
-    it += dt;
-    it += ft;
-    it *= 24;
-    std::cout << "(IntType + DoubleType + FloatType) x 24 = " <<  it << std::endl;
-}
-
-void part4()
-{
-    // ------------------------------------------------------------
-    //                          Power tests
-    // ------------------------------------------------------------
-    Numeric<float> ft1(2);
-    Numeric<double> dt1(2);
-    Numeric<int> it1(2);
-    float floatExp = 2.0f;
-    double doubleExp = 2.0;
-    int intExp = 2;
-    Numeric<int> itExp(2);
-    Numeric<float> ftExp(2.0f);
-    Numeric<double> dtExp(2.0);
-
-    // Power tests with FloatType
-    std::cout << "Power tests with FloatType" << std::endl;
-    std::cout << "pow(ft1, floatExp) = " << ft1 << "^" << floatExp << " = " << ft1.pow(floatExp)  << std::endl;
-    std::cout << "pow(ft1, itExp) = " << ft1 << "^" << itExp << " = " << ft1.pow(itExp)  << std::endl;
-    std::cout << "pow(ft1, ftExp) = " << ft1 << "^" << ftExp << " = " << ft1.pow(ftExp)  << std::endl;
-    std::cout << "pow(ft1, dtExp) = " << ft1 << "^" << dtExp << " = " << ft1.pow(dtExp)  << std::endl;
-    std::cout << "---------------------\n" << std::endl;
-
-    // Power tests with DoubleType
-    std::cout << "Power tests with DoubleType" << std::endl;
-    std::cout << "pow(dt1, doubleExp) = " << dt1 << "^" << doubleExp << " = " << dt1.pow(intExp)  << std::endl;
-    std::cout << "pow(dt1, itExp) = " << dt1 << "^" << itExp << " = " << dt1.pow(itExp)  << std::endl;
-    std::cout << "pow(dt1, ftExp) = " << dt1 << "^" << ftExp << " = " << dt1.pow(ftExp)  << std::endl;
-    std::cout << "pow(dt1, dtExp) = " << dt1 << "^" << dtExp << " = " << dt1.pow(dtExp)  << std::endl;
-    std::cout << "---------------------\n" << std::endl;
-
-    // Power tests with IntType
-    std::cout << "Power tests with IntType" << std::endl;
-    std::cout << "pow(it1, intExp) = " << it1 << "^" << intExp << " = " << it1.pow(intExp)  << std::endl;
-    std::cout << "pow(it1, itExp) = " << it1 << "^" << itExp << " = " << it1.pow(itExp)  << std::endl;
-    std::cout << "pow(it1, ftExp) = " << it1 << "^" << ftExp << " = " << it1.pow(ftExp)  << std::endl;
-    std::cout << "pow(it1, dtExp) = " << it1 << "^" << dtExp << " = " << it1.pow(dtExp)  << std::endl;
-    std::cout << "===============================\n" << std::endl;
-
-    // ------------------------------------------------------------
-    //                          Point tests
-    // ------------------------------------------------------------
-    Numeric<float> ft2(3.0f);
-    Numeric<double> dt2(4.0);
-    Numeric<int> it2(5);
-    float floatMul = 6.0f;
-
-    // Point tests with float
-    std::cout << "Point tests with float argument:" << std::endl;
-    Point p0(ft2, floatMul);
-    p0.toString();
-    std::cout << "Multiplication factor: " << floatMul << std::endl;
-    p0.multiply(floatMul);
-    p0.toString();
-    std::cout << "---------------------\n" << std::endl;
-
-    // Point tests with FloatType
-    std::cout << "Point tests with FloatType argument:" << std::endl;
-    Point p1(ft2, ft2);
-    p1.toString();
-    std::cout << "Multiplication factor: " << ft2 << std::endl;
-    p1.multiply(ft2);
-    p1.toString();
-    std::cout << "---------------------\n" << std::endl;
-
-    // Point tests with DoubleType
-    std::cout << "Point tests with DoubleType argument:" << std::endl;
-    Point p2(ft2, dt2);
-    p2.toString();
-    std::cout << "Multiplication factor: " << dt2 << std::endl;
-    p2.multiply(dt2);
-    p2.toString();
-    std::cout << "---------------------\n" << std::endl;
-
-    // Point tests with IntType
-    std::cout << "Point tests with IntType argument:" << std::endl;
-    Point p3(ft2, dt2);
-    p3.toString();
-    std::cout << "Multiplication factor: " << it2 << std::endl;
-    p3.multiply(it2);
-    p3.toString();
-    std::cout << "---------------------\n" << std::endl;
-}
-
 template<typename Type>
-void cube(Type& t)
+void cube(std::unique_ptr<Type>& t)
 {
-    t *= t * t;
+    auto& v = *t;
+    v = v * v * v;
 }
 
 int main()
